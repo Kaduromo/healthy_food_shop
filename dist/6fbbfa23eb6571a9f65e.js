0272117ad7adac411ccbe4db403cda870b9ae05b
Kaduromo <vitalzsk@gmail.com>
2022-03-09 21:12:11 +0300

    added hamburger, fix adaptations

---
+++ dist/6fbbfa23eb6571a9f65e.js
@@ -0,0 +1,1 @@
+function openModal(){document.getElementById("modal").style.top="0px"}function closeModalMenu(){document.getElementById("modal").style.top="-120%"}
---
+++ src/main.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.getElementById("modal").style.top = "0px";
+}
+
+function closeModalMenu() {
+    document.getElementById("modal").style.top = "-120%";
+}
